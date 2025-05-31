05f7770cf377997db49e62b8f03bd5e6a2d7891d
Hamilton Dias <hamiltondias1997@hotmail.com>
2025-05-31 11:03:48 -0300

    Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import Header from './components/Header';
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div className='relative h-full lg:h-[140vh]'>
-      <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
+    <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
+      <Header />
+
+      <main className='relative pb-24 pl-4 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
           <div className='absolute top-0 left-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
             <Image
