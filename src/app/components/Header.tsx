9b718bf2556220dd21ebd892bdc1f7a893da72d5
Hamilton Dias <hamiltondias1997@hotmail.com>
2025-05-31 11:26:15 -0300

    Header component with classes and state

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,47 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className='fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 lg:px-10 lg:py-6'>
-      <img src='/logo.png' alt='Netflix' width={120} height={120} />
-      <ul>
-        <li>Home</li>
-        <li>TV Shows</li>
-        <li>Movies</li>
-        <li>Latest</li>
-      </ul>
+    <header
+      className={`${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-10 lg:py-6`}
+    >
+      <div className='flex items-center space-x-2 md:space-x-8'>
+        <img src='/logo.png' alt='Netflix' width={120} height={120} />
+        <ul className='hidden md:flex md:space-x-4'>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>Latest</li>
+        </ul>
+      </div>
+
+      <div className='flex items-center space-x-4'>
+        <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
+        <img
+          src='/avatar.png'
+          alt='Netflix'
+          className='h-10 w-10 cursor-pointer rounded'
+        />
+      </div>
     </header>
   );
 }
