8fc4a8c3d61593f816f7c96417790c76975769a3
Hamilton Dias <hamiltondias1997@hotmail.com>
2025-06-09 20:13:42 -0300

    Example paging and loading blocks

---
+++ src/app/settings/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+async function Loading() {
+  return (
+    <div>
+      <h1>Settings Page</h1>
+
+      <div>loading...</div>
+    </div>
+  );
+}
+
+export default Loading;
---
+++ src/app/settings/user/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { getUserInfo } from '../../lib/settings';
+
+async function User() {
+  const { name, email, age } = await getUserInfo();
+
+  return (
+    <div>
+      <h1>User Page</h1>
+
+      <div className='border border-dashed border-red-500 p-4'>
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+        <p>Age: {age}</p>
+      </div>
+    </div>
+  );
+}
+
+export default User;
